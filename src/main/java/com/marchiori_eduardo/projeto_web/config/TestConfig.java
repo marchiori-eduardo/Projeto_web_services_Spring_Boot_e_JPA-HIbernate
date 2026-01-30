432934f5873a4ca5c60337a1264ba2fe55a0919c
marchiori-eduardo <marchiorimarchiori2@gmail.com>
2026-01-30 19:24:02 -0300

    Payment, one-to-one association

--- src/main/java/com/marchiori_eduardo/projeto_web/config/TestConfig.java
+++ src/main/java/com/marchiori_eduardo/projeto_web/config/TestConfig.java
@@ -75,5 +75,10 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        //para salvar um objeto dependente com relação OneToOne, usa-se a associação de mão dupla em memória
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
+
     }
 }
