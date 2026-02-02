b619b50b2779f1e62008a766f953e73326a21d94
marchiori-eduardo <marchiorimarchiori2@gmail.com>
2026-02-02 14:38:25 -0300

    Exception handling - update

--- src/main/java/com/marchiori_eduardo/projeto_web/services/UserService.java
+++ src/main/java/com/marchiori_eduardo/projeto_web/services/UserService.java
@@ -4,6 +4,7 @@ import com.marchiori_eduardo.projeto_web.entities.User;
 import com.marchiori_eduardo.projeto_web.repositories.UserRepository;
 import com.marchiori_eduardo.projeto_web.services.exceptions.DatabaseException;
 import com.marchiori_eduardo.projeto_web.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj); //atualizando o a entidade entity com base no objeto obj
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj); //atualizando o a entidade entity com base no objeto obj
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
